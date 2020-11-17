1d15ace3a9ae6889443386cafc738f77269d821f
wayneliu0512 <wayneliu0512@gmail.com>
2020-11-17 22:00:44 +0800

    second commit

---
+++ hello/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index (request):
+    return HttpResponse("Hello !")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name
+    })
